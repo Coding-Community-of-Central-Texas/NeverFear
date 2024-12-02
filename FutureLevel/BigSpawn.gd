aa5d9b7ef1c5d402a5b2f81129b1e4d7f7bdc488
OSCCCT <161489452+OSCCCT@users.noreply.github.com>
2024-12-01 18:57:08 -0600

    Enemies

---
+++ FutureLevel/BigSpawn.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+
+
+const ROBBIE = preload("res://Scenes/ThreeHitter.tscn")
+const TANK = preload("res://Scenes/TankLegacy.tscn")
+
+enum Big {
+	ROBBIE,
+	TANK
+}
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):  # Ensure the body is part of the player group
+		var character_to_spawn: PackedScene
+		if randi() % 2 == 0:  # Randomly select between ROBBIE and TANK
+			character_to_spawn = ROBBIE
+		else:
+			character_to_spawn = TANK
+				
+		# Instance the selected character and add it to the scene
+		var character_instance = character_to_spawn.instantiate()
+		add_child(character_instance)
+				
+		 # Position the character at the Area2D's location (or adjust as needed)
+		character_instance.position = position
---
+++ FutureLevel/TankLegacy.gd
@@ -0,0 +1,88 @@
+extends CharacterBody2D
+
+
+var health = 70.0
+var knockback_strength = 500.0
+var knockback_duration = 0.7
+var knockback_timer = 0.0  # Keeps track of the knockback time
+var knockback_velocity = Vector2.ZERO  # Stores knockback velocity
+
+@onready var tank: Node2D = %Tank
+@onready var ground_ray_right: RayCast2D = $GroundRayRight
+@onready var ground_ray_left: RayCast2D = $GroundRayLeft
+@onready var left_wall_ray: RayCast2D = $LeftWallRay
+@onready var right_wall_ray: RayCast2D = $RightWallRay
+
+
+
+var move_speed = 100.0  # Movement speed
+var max_distance = 150.0  # The max distance to move before reversing direction
+var direction = 1  # 1 for right, -1 for left
+
+
+var platform_edge_offset = 20.0  # Distance from the edge of the platform to reverse direction
+var starting_position = Vector2.ZERO
+
+
+func _ready():
+	%Tank.play_walk()
+	starting_position = global_position
+
+func _physics_process(_delta):
+	if not is_on_floor():
+		velocity += get_gravity() * _delta
+
+	if knockback_timer > 0:
+		velocity = knockback_velocity
+		knockback_timer -= _delta # decrease knockback time 
+	else:
+		back_and_forth_movement()
+	move_and_slide()
+
+
+func take_damage():
+	health -= 10.0
+	%Tank.play_hurt()
+	
+	var knockback_direction = (global_position - Global.player.global_position).normalized()
+	knockback_velocity = knockback_direction * knockback_strength
+	knockback_timer = knockback_duration
+	
+	if health == 0.0:
+		_die()
+
+func _die():
+	
+	const BOOM = preload("res://Scenes/RobbieBoom.tscn")
+	var new_Boom = BOOM.instantiate()
+	new_Boom.global_position = global_position
+	get_parent().add_child(new_Boom)
+	
+	const CASH = preload("res://Scenes/Cash.tscn")
+	var new_Cash = CASH.instantiate()
+	new_Cash.randomize_power_up()
+	new_Cash.global_position = global_position 
+	get_tree().current_scene.call_deferred("add_child", new_Cash)
+	queue_free()
+
+func back_and_forth_movement():
+	velocity.x = direction * move_speed
+
+	if is_near_platform_edge():
+		direction = -direction  # Reverse direction to avoid falling off the platform
+	elif is_near_wall():
+		direction = -direction
+
+func is_near_wall() -> bool:
+	if direction == 1:
+		return right_wall_ray.is_colliding()
+	elif direction == -1:
+		return left_wall_ray.is_colliding()
+	return false 
+
+func is_near_platform_edge() -> bool:
+	if direction == 1:
+		return !ground_ray_right.is_colliding()
+	elif direction == -1: 
+		return !ground_ray_left.is_colliding()
+	return false
