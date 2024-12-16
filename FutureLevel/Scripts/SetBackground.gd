da89267eee4b5978ccc84797aa908f1649323596
OSCCCT <161489452+OSCCCT@users.noreply.github.com>
2024-12-16 13:27:44 -0600

    commit

--- FutureLevel/Scripts/SetBackground.gd
+++ FutureLevel/Scripts/SetBackground.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 @onready var pipland: Sprite2D = $"../TileMap/CityBack/Background/Pipland"
 
-
+func _ready() -> void:
+	pipland.visible = false
 
 func _on_body_entered(body: Node2D) -> void:
 	pipland.visible = true
