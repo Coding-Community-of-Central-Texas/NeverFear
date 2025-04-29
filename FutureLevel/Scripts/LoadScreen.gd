f58ce92bad6bb02d4986ec649d7d2703a9028cdd
OSCCCT <stephen@genuineresult.com>
2025-04-29 18:38:39 -0500

    Google Play Services Plugin

--- FutureLevel/Scripts/LoadScreen.gd
+++ FutureLevel/Scripts/LoadScreen.gd
@@ -1,4 +1,13 @@
 extends Node
 
+var gpgs
+
+func _ready():
+	gpgs = Engine.get_singleton("GodotPlayGamesServices")
+	if gpgs:
+		gpgs.sign_in()
+	else:
+		print("Google Play Services plugin not found!")
+
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://Scenes/HubWorld.tscn")
--- FutureLevel/Scripts/WinScreen.gd
+++ FutureLevel/Scripts/WinScreen.gd
@@ -5,28 +5,23 @@ extends CanvasLayer
 @onready var enimies_killed: Label = $Win/EnimiesKilled
 @onready var cash_collected: Label = $Win/CashCollected
 @onready var run_time: Label = $Win/RunTime
-@onready var data_vault: Sprite2D = $Win/DataVault
-@onready var restart: Sprite2D = $Win/Restart
+
 
 func _process(_delta):
 	run_time.text = "run time: %s" % Global.legacy.level_time()
 	enimies_killed.text = "bots vaporized: %d" % GameManager.current_kills
-	cash_collected.text = "cash collected: $ %s" % GameManager.game_cash
+	cash_collected.text = "cash collected:  %s" % GameManager.game_cash
 
 func _on_stats_link_pressed() -> void:
 	audio_stream_player_2d.play()
-	data_vault.modulate = Color(0.5, 0.5, 0)  # Set to red
 	await get_tree().create_timer(0.2).timeout  # Wait for 0.1 seconds
-	data_vault.modulate = Color(1, 1, 1)  # Reset to normal
 	Engine.time_scale = 1.0
 	Global.reset_game_state()
 	get_tree().change_scene_to_file("res://Scenes/PerformanceIndex.tscn")
 
 func _on_restart_pressed() -> void:
 	audio_stream_player_2d.play()
-	restart.modulate = Color(0.5, 0.5, 0)  # Set to red
 	await get_tree().create_timer(0.2).timeout  # Wait for 0.1 seconds
-	restart.modulate = Color(1, 1, 1)  # Reset to normal
 	Engine.time_scale = 1.0
 	Global.reset_game_state()
 	get_tree().reload_current_scene()
